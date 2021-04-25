c8686d4eee6206c9aa9df9c5eb915931dbc7dc80
Larissa Silva <vinitrajano@hotmail.com>
2021-04-25 11:27:36 -0300

    finalizado

---
+++ README.md
@@ -0,0 +1,28 @@
+# NLW-5_Node.js
+
+ <img src="https://nextlevelweek.com/og/next-level-week.png"></img>
+
+
+ **Aplicação desenvolvida durante 20/04/20 a 25/04/20 da NLW_5 da Rocketseat na Trilha** ![Node](https://img.shields.io/badge/-Node.js-333333?style=flat&logo=node.js)
+ 
+
+
+### 🛠 Tecnologias
+
+As seguintes ferramentas foram usadas no projeto até o momento:
+
+- [Node.js]()
+- [Gerenciador NPM]()
+- [Express.js]()
+- [Sqlite3]()
+- [Typeorm]()
+- [Typescript]()
+- [Insominia]()
+- [Beekeeper Studio]()
+
+
+
+
+#### Autor:
+
+- [Carlos Trajano](https://github.com/CarllosT)
---
+++ src/services/UsersService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+
+class UsersService {
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({ email });
+      
+        return user;
+      }
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string){
+    
+        //verificar se usuario existe
+        const userExists = await this.usersRepository.findOne({
+            email 
+        });
+
+        //se existir retornar usuario.
+        if (userExists) {
+            return userExists;
+        }
+            const user = this.usersRepository.create({
+                email,
+            });
+
+            await this.usersRepository.save(user);
+
+            //se não existir, salvar no db
+
+            return user;
+
+
+
+    }
+}
+
+export {UsersService}
